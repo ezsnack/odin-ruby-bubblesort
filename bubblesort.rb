0c8c7c3bda9f3b31215b068ed2e13b180adba0d5
ezsnack <204544689+ezsnack@users.noreply.github.com>
2026-02-02 23:56:48 +0900

    Add mutating version (bubble_sort!)

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,5 +1,6 @@
+# does not modify original array
 def bubble_sort(input)
-  arr = Array.new(input) # don't modify the original array
+  arr = Array.new(input)
   len = arr.length
   stop = 1
   (len - 1).times do
@@ -15,6 +16,19 @@ def bubble_sort(input)
   arr
 end
 
-# tests
-p bubble_sort([7,6,5,4,3,2,9,3,9,3,5,2,4])
-p bubble_sort([5,4,3,2,1,0,-1])
+# modifies original array
+def bubble_sort!(arr)
+  len = arr.length
+  stop = 1
+  (len - 1).times do
+    (len - stop).times do |i|
+      if arr[i] > arr[i + 1]
+        tmp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = tmp
+      end
+    end
+    stop += 1
+  end
+  arr
+end
